6a7576e5122be9ff6040149828c155898db74e65
lahyeok <thisisdbdb1@gmail.com>
2024-02-15 09:40:51 +0900

    klh gitignore 추가

--- lec_management.js
+++ lec_management.js
@@ -1,4 +1,3 @@
-
 const mysql = require('mysql');
 const Input = require('./userInput');
 
@@ -10,17 +9,16 @@ let connection = mysql.createConnection({
     database: process.env.DB_NAME,
 });
 
-// 비동기 방식으로 진행, 데이터 삭제를 입력받음
+
 async function deleteData() {
     console.log('삭제할 행 번호 입력 : ');
-// await 으로 해당 줄 동기적으로 실행
     const id = await Input.getUserInput();
-// 쿼리를 사용하여 해당 번호의 행 삭제
+
     const sql = `delete from student where id = ?`;
-// sql 쿼리 작성
+
     connection.query(sql, [id], (error, results) => {
         if (error) return console.error(error.message);
-// affectedRows 삭제된 데이터의 개수를 나타냄, 결과처리
+
         if (results.affectedRows === 0) {
             console.log(`번호 ${id}에 해당하는 데이터가 없습니다.`);
         } else {
@@ -28,5 +26,3 @@ async function deleteData() {
         }
     });
 }
-
-module.exports = deleteData;
