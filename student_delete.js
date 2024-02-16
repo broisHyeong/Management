67bb5db63c18c0a376ff0db5b473ad78393f59c8
lahyeok <thisisdbdb1@gmail.com>
2024-02-16 10:43:15 +0900

    delete 파일 수정

--- student_delete.js
+++ student_delete.js
@@ -1,48 +1,59 @@
-const readline = require('readline');
+//student_delete.js
+const Input = require("./userInput");
 const mysql = require('mysql');
 
-// 사용자 입력 처리
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+//DB접속
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 async function main(){
-  rl.question('삭제할 학번 입력: ', (studentId) => {
-    // MySQL 연결
-      const connection = mysql.createConnection({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-  });
-  
   connection.connect();
-  
+
+  let exist = true;
+  let student_id;
+
+  //while문으로 반복실행
+  while (exist) {
+    //학번입력
+    console.log(`삭제할 학번을 입력하세요`);
+    student_id = await Input.getUserInput();
+    let selectsql = `SELECT * FROM student WHERE student_id = ?`;
+
+    //입력된 학번이 테이블에 있는지 확인
+    connection.query(selectsql, [student_id], (selectErr, results) => {
+      if (selectErr) {
+        console.log(selectErr.message);
+      }
+      //학번이 없으면 다시 입력하도록 함
+      if (results.length === 0) {
+        console.log(
+          `입력한 학번 ${student_id}에 해당하는 데이터가 없습니다. 다시 입력해주세요`
+        );
+      } else {
+        exist = false;
+      }
+    });
+    await wait(1000);
+  }
+  //학번이 있으면 작업 진행
+  if (!exist) {
     // SQL 쿼리 생성
-  const sql = `DELETE FROM student WHERE student_id = ${studentId}`;
-  
+  const sql = `DELETE FROM student WHERE student_id = ${student_id}`;
+
     // 쿼리 실행
   connection.query(sql, (err, result) => {
-      if (err) {
-      console.error(err.message);
-      connection.end();
-      return;
-      }
-  
-      // 결과 처리
-      if (result.affectedRows === 0) {
-      console.log('삭제할 학번이 없습니다.');
-      } else {
+      if (err) console.error(err.message);
       console.log(`${result.affectedRows}개의 행이 삭제되었습니다.`);
-      }
-  
-      connection.end();
-      rl.close();
       });
-  });
+  }
 }
-main();
+
+const wait = (timeToDelay) =>
+  new Promise((resolve) => setTimeout(resolve, timeToDelay));
 
 module.exports = {main};
